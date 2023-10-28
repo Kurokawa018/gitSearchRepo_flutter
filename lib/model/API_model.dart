19b0f7bc1a20485adc56cb5560cd8a27a8b88ee7
Kurokawa018 <kurokawa.kazuma018@gmail.comm>
2023-10-28 16:50:59 +0900

    APIの実装

--- lib/model/API_model.dart
+++ lib/model/API_model.dart
@@ -2,9 +2,11 @@ import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
 
-class GithubClient {
+class GithubModel {
   Future<List<dynamic>?> get(String query) async {
+    //APIリクエスト
     var url = 'https://api.github.com/search/repositories?q=$query';
+
     var response = await http.get(Uri.parse(url));
     if (response.statusCode == 200) {
       var jsonResponse = json.decode(response.body);
