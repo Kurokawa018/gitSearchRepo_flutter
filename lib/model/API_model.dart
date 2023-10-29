e478eb4b94a10a6a5fd803ffb685129b1f7ebe53
Kurokawa018 <kurokawa.kazuma018@gmail.comm>
2023-10-29 10:57:33 +0900

    APIリクエストの状態をriverpodで管理する

--- lib/model/API_model.dart
+++ lib/model/API_model.dart
@@ -1,16 +1,50 @@
 import 'dart:convert';
 import 'dart:io';
+import 'package:flutter/cupertino.dart';
 import 'package:http/http.dart' as http;
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-class GithubModel {
-  Future<List<dynamic>?> get(String query) async {
-    //APIリクエスト
+final githubProvider = ChangeNotifierProvider(
+        (ref) => GithubModel()
+);
+
+class GithubModel extends ChangeNotifier {
+  bool isLoading = false;
+  List<dynamic> items = [];
+
+  Future<void> search(String query) async {
+    var result = await fetchRepositories(query);
+    //nullチェック
+    items = result!;
+    isLoading = false;
+    notifyListeners();
+  }
+
+
+  Future<List<dynamic>?> fetchRepositories(String query) async {
+    //Loadingの開始
+    startLoading();
+    //リクエストの開始
     var url = 'https://api.github.com/search/repositories?q=$query';
 
     var response = await http.get(Uri.parse(url));
+    print("===" + response.body + "========");
     if (response.statusCode == 200) {
       var jsonResponse = json.decode(response.body);
       return jsonResponse['items'];
+    } else {
+      throw Exception('Failed to load repositories');
     }
+    //Loadingの収量
+    endLoading();
+  }
+  void startLoading() {
+    isLoading = true;
+    notifyListeners();
+  }
+  void endLoading() {
+    isLoading = false;
+    notifyListeners();
   }
 }
+
