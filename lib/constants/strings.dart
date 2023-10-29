c8822a637272e9b30a85b3be3d63cea0bbe7584b
Kurokawa018 <kurokawa.kazuma018@gmail.comm>
2023-10-29 18:41:26 +0900

    noResultsを改行

--- lib/constants/strings.dart
+++ lib/constants/strings.dart
@@ -8,4 +8,4 @@ const String loadingText = "ローディング中";
 const String errorText = '申し訳ございませんが、再度検索してください';
 const String buttonText = "検索";
 const String hintText = "キーワードを入力";
-const String noResults = "検索結果がありませんでした。違うキーワードで検索してください";
+const String noResults = "検索結果がありませんでした。\n 違うキーワードで検索してください";
